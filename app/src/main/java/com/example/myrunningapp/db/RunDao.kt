04a19a97ee5846cfe6c27fcca15bf1eeae38e839
takieddine12 <algeriahero22@gmail.com>
2020-07-07 15:42:41 +0100

    My Running App

---
+++ app/src/main/java/com/example/myrunningapp/db/RunDao.kt
@@ -0,0 +1,41 @@
+package com.example.myrunningapp.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface RunDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRun(runModel: RunModel)
+
+    @Delete
+    suspend fun deleteRun(runModel: RunModel)
+
+    @Query("SELECT * FROM runningtable ORDER BY timeStamp DESC")
+    fun getRunsSortedByDate() : LiveData<List<RunModel>>
+
+    @Query("SELECT * FROM runningtable ORDER BY avgSpeed ")
+    fun getRunsSortedByAvgSpeed() : LiveData<List<RunModel>>
+
+    @Query("SELECT * FROM runningtable ORDER BY disntanceInMeters")
+    fun getRunsSortedByDistanceInMeters() :  LiveData<List<RunModel>>
+
+    @Query("SELECT * FROM runningtable ORDER BY timeInMillis")
+    fun getRunsSortedByTimeInMillis() : LiveData<List<RunModel>>
+
+    @Query("SELECT * FROM runningtable ORDER BY caloriesBurned")
+    fun getRunsSortedByCaloriesBurned() : LiveData<List<RunModel>>
+
+    @Query("SELECT SUM(timeStamp) FROM runningtable")
+    fun getTotalTimeStamp() : LiveData<Long>
+
+    @Query("SELECT SUM(caloriesBurned) FROM runningtable")
+    fun getTotalCaloriesBurned() : LiveData<Int>
+
+    @Query("SELECT SUM(timeInMillis) FROM runningtable")
+    fun getTotalTimeInMillis() : LiveData<Long>
+
+    @Query("SELECT AVG(avgSpeed) FROM runningtable")
+    fun getTotalAvgSpeeds() : LiveData<Float>
+}
---
+++ app/src/main/java/com/example/myrunningapp/db/RunModel.kt
@@ -0,0 +1,21 @@
+package com.example.myrunningapp.db
+
+import android.graphics.Bitmap
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import org.jetbrains.annotations.NotNull
+
+@Entity(tableName = "runningtable")
+data class RunModel(
+    var bitMap : Bitmap? = null,
+    var timeStamp : Long = 0L, //this is when the run took place
+    var avgSpeed : Float = 0F,
+    var disntanceInMeters : Int = 0,
+    var timeInMillis : Long = 0L,  //this is for how long the run was
+    var caloriesBurned : Int = 0
+) {
+
+    @PrimaryKey(autoGenerate = true)
+    @NotNull
+    private var runningID : Int? = null
+}
